6aa8e74366ac853b944436179ad2f5ea4691e51a
Robert Schmid <robert.schmid@student.hpi.uni-potsdam.de>
2014-05-01 20:12:21 +0200

    Update README and branch for screenshots

--- build-support/screenshots.sh
+++ build-support/screenshots.sh
@@ -1,4 +1,4 @@
-if [ "$SCREENSHOT" = "true" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
+if [ "$SCREENSHOT" = "true" ] && [ "$TRAVIS_BRANCH" = "just-for-fun/build-screenshots" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
 	PNG=$(find -name "scamper.png")
 	echo $PNG
 	curl --form-string "token=$SCREENSHOT_KEY" --form "file=@$PNG" "https://swt-scamper.azurewebsites.net/screenshot/"
