c7776a612f7d1b0f16d64b7c8eab4bd08b0e019a
Robert Schmid <robert.schmid@student.hpi.uni-potsdam.de>
2014-05-02 02:38:52 +0200

    Make screenshot test always pass, set screenshot output path in script

--- build-support/screenshots.sh
+++ build-support/screenshots.sh
@@ -1,5 +1,3 @@
 if [ "$SCREENSHOT" = "true" ] && [ "$TRAVIS_BRANCH" = "just-for-fun/build-screenshots" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
-	PNG=$(find -name "scamper.png")
-	echo $PNG
-	curl --form-string "token=$SCREENSHOT_KEY" --form "file=@$PNG" "https://swt-scamper.azurewebsites.net/screenshot/"
+	curl --form-string "token=$SCREENSHOT_KEY" --form "file=@${BUILD_PATH}/travisCI/scamper.png" "https://swt-scamper.azurewebsites.net/screenshot/"
 fi
