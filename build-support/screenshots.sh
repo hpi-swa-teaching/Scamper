8ae7136ddfc389e60e00a03f96467ff562f3a83d
Robert Schmid <robert.schmid@student.hpi.uni-potsdam.de>
2014-05-02 12:51:37 +0200

    Configure to take screenshots for builds of master [ci skip]

--- build-support/screenshots.sh
+++ build-support/screenshots.sh
@@ -1,3 +1,3 @@
-if [ "$SCREENSHOT" = "true" ] && [ "$TRAVIS_BRANCH" = "just-for-fun/build-screenshots" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
+if [ "$SCREENSHOT" = "true" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
 	curl --form-string "token=$SCREENSHOT_KEY" --form "file=@${BUILD_PATH}/travisCI/scamper.png" "https://swt-scamper.azurewebsites.net/screenshot/"
 fi
