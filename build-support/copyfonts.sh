9f2dd4cc92cf157f3237eefc0313956a13833433
MrSerth <MrSerth@users.noreply.github.com>
2014-06-13 14:00:31 +0200

    Change unix permissions of all files included in the fonts directory and print all information available.

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,2 +1,4 @@
 mkdir -p -v ${BUILD_PATH}/travisCI/Scamper/fonts
 cp -R -v $PROJECT_HOME/build-support/fonts ${BUILD_PATH}/travisCI/Scamper/
+chmod -R 777 ${BUILD_PATH}/travisCI/Scamper/fonts/*
+ls -lisa ${BUILD_PATH}/travisCI/Scamper/fonts/
