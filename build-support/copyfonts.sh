1ff4fc002d992eb995245aa091ea3093a1c4b50a
Stefan Neubert <Stefan.Neubert@student.hpi.uni-potsdam.de>
2014-06-15 11:03:08 +0200

    Use custom testTravisCI.sh script to stop travis from deleting the output folder.

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,4 +1,3 @@
 mkdir -p -v ${BUILD_PATH}/travisCI/Scamper/fonts
 cp -R -v $PROJECT_HOME/build-support/fonts ${BUILD_PATH}/travisCI/Scamper/
 chmod -R 777 ${BUILD_PATH}/travisCI/Scamper/fonts/*
-ls -lisa ${BUILD_PATH}/travisCI/Scamper/fonts/
