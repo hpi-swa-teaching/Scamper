5cf6a02917d9b12e9152d459c873d9f41f77479b
Matthias Radscheit <matthias@radscheit.de>
2016-05-21 18:26:25 +0200

    3rd try t fox copyfonts.sh in order to have a valid travis build

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,3 +1,3 @@
 mkdir -p -v $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts
-cp -vR $PROJECT_HOME/build-support/fonts/* $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts
+cp -vR $HOME/build-support/fonts/* $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts
 chmod -R 777 $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts/*
