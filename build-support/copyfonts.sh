0e511f0f8dbdb38a469a0c1b2a71d9c129e1c213
Matthias Radscheit <matthias@radscheit.de>
2016-05-25 21:19:20 +0200

    19th try to fix copyfonts

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -7,4 +7,3 @@ fi
 
 cp -vR $TRAVIS_BUILD_DIR/build-support/fonts/* ${BUILD_PATH}/travisCI/Scamper/fonts
 chmod -R 777 ${BUILD_PATH}/travisCI/Scamper/fonts/*
-ls -l  /home/travis/smalltalkCI-master/_builds
