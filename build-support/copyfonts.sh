f1a878130cd8e00d45785c5c0161a581d061ea57
Matthias Radscheit <matthias@radscheit.de>
2016-05-25 21:32:12 +0200

    21st try to fix copyfonts

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,10 +1,9 @@
-chmod -R 777 ${BUILD_PATH}
-mkdir -vp ${BUILD_PATH}/travisCI/Scamper/fonts
+mkdir -vp $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts
 pwd
 if [! [-d $TRAVIS_BUILD_DIR/build-support/fonts/ ]]
 	then
 		echo "font dir not exists."
 fi
 
-cp -vR $TRAVIS_BUILD_DIR/build-support/fonts/* ${BUILD_PATH}/travisCI/Scamper/fonts
-chmod -R 777 ${BUILD_PATH}/travisCI/Scamper/fonts/*
+cp -vR $TRAVIS_BUILD_DIR/build-support/fonts/* $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts
+chmod -R 777 $TRAVIS_BUILD_DIR/travisCI/Scamper/fonts/*
